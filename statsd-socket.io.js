4dc4fb856228be48a92830c219eb02bd3c586bd1
Chris Hoffman <choffman@chathamfinancial.com>
2012-05-17 10:42:59 -0400

    Splitting stats strings into nested objects to subscribe on any level

--- statsd-socket.io.js
+++ statsd-socket.io.js
@@ -5,17 +5,43 @@ var debug;
 
 var emit_stats = function (socket, ts, metrics) {
   socket.emit('timestamp', ts);
+
+  //Convert dot separated metrics into nested object
+  ['gauges','timers','counters'].forEach(function (metric_type) {
+    metrics[metric_type] = deepen(metrics[metric_type])[metric_type];
+  });
+
   socket.get('stats', function (err, stats) {
     stats.forEach(function (stat) {
       if (stat == 'all') {
         socket.emit('all', metrics);
       }
       else {
-        var stat_val = vm.runInNewContext(stat, metrics);
-        socket.emit(stat, stat_val);
+        try {
+          var stat_val = vm.runInNewContext(stat, metrics);
+          socket.emit(stat, stat_val);
+        }
+        catch (e) {
+          socket.emit(stat, undefined);
+        }
       }
     });
   });
+};
+
+var deepen = function (o) {
+  var oo = {}, t, parts, part;
+  for (var k in o) {
+    t = oo;
+    parts = k.split('.');
+    var key = parts.pop();
+    while (parts.length) {
+      part = parts.shift();
+      t = t[part] = t[part] || {};
+    }
+    t[key] = o[k];
+  }
+  return oo;
 };
 
 exports.init = function (startup_time, config, events) {
@@ -32,7 +58,7 @@ exports.init = function (startup_time, config, events) {
     io.set('log level', 4);
   }
   else {
-    io.set('log level', 1)
+    io.set('log level', 1);
   }
 
   io.sockets.on('connection', function (socket) {
